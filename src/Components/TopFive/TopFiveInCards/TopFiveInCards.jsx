8fc40f52e76cd950268e4fdd55819bbfac589b36
Jose Franny Rodriguez <icancode911@gmail.com>
2019-07-13 10:02:58 -0400

    feat(nba-app): added redux connection and other UI fixes

--- src/Components/TopFive/TopFiveInCards/TopFiveInCards.jsx
+++ src/Components/TopFive/TopFiveInCards/TopFiveInCards.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import Button from '../../Button/Button';
 import NbaConsts from '../../../lib/nbaconstants';
 import PropTypes from 'prop-types';
 
-const TopFiveInCards = ({players}) => {
-  const {stats} = NbaConsts;
-
+const TopFiveInCards = ({players, onClick}) => {
+  const { stats } = NbaConsts;
+  const btnText = "Remove";
   return (
     <div className = 'top-five-in-cards'>
       {players.map((player, idx) => {
         return(
          <div className = 'player-card' key={idx}>
-            <img className = 'player-img' src='/mario.png' alt = ''/>
+            <img className = 'player-img' src='/mario.png' alt = 'placeholder' />
             <div className = 'player-stats'>
                 <div className = 'prop-stats'>{stats.ppg}{player.fgp}</div>
                 <div className = 'prop-stats'>{stats.apg}{player.ast}</div>
                 <div className = 'prop-stats'>{stats.rpg}{player.rbpg}</div>
                 <div className = 'prop-stats'>{stats.ftp}{player.ftp}</div>
+                <Button onClick={() => alert('HEY')} text={btnText} rvmBtn />
             </div>
          </div>
         )}
---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import {
+  DISPLAY_ROW_OR_COLUMN,
+  // SEARCH_PLAYER,
+} from './constants';
+
+export const displayTopFive = () => ({
+    type: DISPLAY_ROW_OR_COLUMN,
+  });
