ee843d85576d0f25ebc025e28242a59b9e7e0fd3
Jose Franny Rodriguez <icancode911@gmail.com>
2019-07-14 23:17:04 -0400

    feat(nba-app): add NBA api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
+const NBA = require('nba');
+
 import {
   DISPLAY_ROW_OR_COLUMN,
-  // SEARCH_PLAYER,
+  SEARCH_FOR_PLAYER
 } from './constants';
 
 export const displayTopFive = () => ({
     type: DISPLAY_ROW_OR_COLUMN,
   });
+
+export const searchForPlayer = (player) => ({
+  type: SEARCH_FOR_PLAYER,
+  payload:
+})
